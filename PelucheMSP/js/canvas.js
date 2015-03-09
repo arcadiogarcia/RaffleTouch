d5a22fae260a426ccf68fcac2159122b1d1fa1c0
Arcadio García <49.pyme@garciamontoro.com>
2015-03-09 10:40:44 +0100

    First version
    This was the version used in the dotNetConference Spain 2015

---
+++ PelucheMSP/js/canvas.js
@@ -0,0 +1,60 @@
+﻿var canvas;
+var context;
+
+
+//These are the words that can be displayed
+var presets = ["Students", "Curiosity","Technology","Learn","Create","Devices","Languages","Programming","Collaboration","Events","Microsoft","C#","Visual Studio","JavaScript","Universal","Windows","Cloud","IoT","Apps",".NET","Azure","ASP .NET","XAML","HTML5","Hour of Code","Web","Powershell","Windows Server","Videogames","Xbox One","Developers","Robotics","Kinect","Spain","MVVM","Windows Phone","Xamarin","SQL","Active Directory","Office","Cloud first","Mobile first"];
+//These are the word that are currently being displayed
+var texts = [];
+
+//Set up everything
+function initcanvas(){
+     canvas = document.getElementById("canvas");
+     context = canvas.getContext("2d");
+     resize();
+}
+
+//To adapt to the screen
+function resize() {
+    context.width = canvas.width = window.innerWidth;
+    context.height = canvas.height = window.innerHeight;
+}
+
+//The main loop that renders the words
+function canvasloop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    //For each word,draw
+    context.textAlign = 'center';
+    for (var i = 0; i < texts.length; i++) {
+        context.font = Math.round(texts[i].size) + 'px Segoe UI';
+        texts[i].size *= 0.993;
+        context.fillStyle = texts[i].color;
+        context.globalAlpha = texts[i].alpha;
+        context.fillText(texts[i].text, Math.round(texts[i].x), Math.round(texts[i].y));
+        texts[i].x += texts[i].vx;
+        texts[i].y += texts[i].vy;
+        texts[i].alpha *= 0.993;
+        if (texts[i].alpha < 0.02) {
+            texts.splice(i, 1);
+            i--;
+        }
+    }
+
+    //Sometimes we push a new word
+    //0.92 works fine, but can be changed if you want more/less words
+    if (Math.random() > 0.92) {
+        texts.push({
+            text: presets[Math.floor(Math.random() * presets.length)],
+            x: Math.floor(Math.random() * canvas.width),
+            y: Math.floor(Math.random() * canvas.height),
+            vx: (Math.random() - 0.5),
+            vy: (Math.random() - 0.5),
+            size: Math.random() * 50 + 50,
+            alpha: 1,
+            color: "#FFF"
+        });
+    }
+
+}
---
+++ PelucheMSP/js/sorteo.js
@@ -0,0 +1,89 @@
+﻿//The current state of the app
+//0-Idle/Waiting for input
+//1-The user pressed the buttons but still doesnt know the result
+//2-The result is shown on screen
+var state = 0;
+//Indicates the chance of winning, should be adjusted, always between [0,1]
+var probabilidad = 0.5;
+//Those are the messages, customize as you want
+var strings = [];
+strings[0] = "Calculando";
+strings[1] = "¡Has ganado!";
+strings[2] = "No has tenido suerte :(";
+strings[3] = "¡Quiero mi peluche MSP!";
+
+function sorteoSetUp() {
+    //Set the starting string
+    document.getElementById("content").innerText = strings[3];
+}
+
+//On mousedown handler
+//Change the color when holding down
+//(I should use CSS but this somehow made more sense in the moment)
+function hold() {
+    document.getElementById("content").style.backgroundColor = "rgba(155,0,185,0.7)";
+}
+
+//On mouseup handler
+//Choose the action depending on the state
+function clickbox() {
+    switch (state) {
+        //When in 'idle' state
+        case 0:
+            sortear();
+            break;
+        //When showing the result of the previous game
+        case 2:
+            restablecer()
+            break;
+    }
+}
+
+//This just sets the text and color, and timer for the dots and the function to really calculate the result
+function sortear() {
+    if (state == 0) {
+        document.getElementById("content").style.color = "rgba(255,255,255,1)";
+        state = 1;
+        document.getElementById("content").innerText = strings[0];
+        document.getElementById("content").style.backgroundColor = "rgba(255,255,255,0.6)";
+        for (var i = 0; i < 10; i++) {
+            window.setTimeout(anadirPunto, i * 1400 / 10);
+        }
+        window.setTimeout(mostrarResultado, 1500);
+    }
+}
+
+//Quite self descriptive
+function anadirPunto() {
+    if (state == 1) {
+        document.getElementById("content").innerText += ". ";
+    }
+}
+
+//This function actually "throws the dice" and shows the result
+function mostrarResultado() {
+    if (state == 1) {
+        state = 2;
+        if (Math.random() <= probabilidad ) {
+            document.getElementById("content").innerText = strings[1];
+            document.getElementById("content").style.backgroundColor = "rgba(0,255,0,0.6)";
+        } else {
+            document.getElementById("content").innerText = strings[2];
+            document.getElementById("content").style.backgroundColor = "rgba(255,0,0,0.6)";
+        }
+    }
+}
+
+//This function 'resets' the app for the next user
+function restablecer() {
+    if (state == 2) {
+        state = 0;
+        document.getElementById("content").innerText = strings[3];
+        document.getElementById("content").style.backgroundColor = "rgba(60,200,244,0.6)";
+        document.getElementById("content").style.color = "rgba(60,114,188,1)";
+    }
+}
+
+
+
+
